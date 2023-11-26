b092e88a8fddc335165384bc7e0b67b8bb156bfc
Edgar <edgar_khanamiryan@helix.am>
2023-11-26 13:24:18 +0400

    routes

--- task/routes/api.php
+++ task/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::middleware('guest')->group(function () {
+    Route::post('/register', [\App\Http\Controllers\Api\RegistrationController::class, 'register']);
+    Route::post('/login', [\App\Http\Controllers\Api\LoginController::class, 'login']);
 });
--- task/routes/web.php
+++ task/routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProfileResetController;
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\UserImageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +20,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [IndexController::class, 'index']);
+
+    Route::post('/register', [RegistrationController::class, 'register']);
+    Route::post('/login', [LoginController::class, 'login']);
+
+    Route::get('/reset/password', [ProfileResetController::class, 'resetPasswordIndex']);
+    Route::get('/reset/password/confirm/{token}', [ProfileResetController::class, 'resetPasswordEdit']);
+    Route::post('/reset/password/store', [ProfileResetController::class, 'passwordResetStore']);
+    Route::post('/reset/password/update', [ProfileResetController::class, 'passwordResetUpdate']);
+
+    Route::get('/reset/email', [ProfileResetController::class, 'resetEmailIndex']);
+    Route::get('/reset/email/confirm', [ProfileResetController::class, 'resetEmailEdit']);
+    Route::get('/reset/email/sent', [ProfileResetController::class, 'resetEmailSent']);
+    Route::post('reset/email/store', [ProfileResetController::class, 'emailResetStore']);
+    Route::post('/reset/email/update', [ProfileResetController::class, 'emailResetUpdate']);
+
+    Route::get('/reset/successful', [ProfileResetController::class, 'successfulReset']);
 });
+
+Route::middleware('auth')->group(function () {
+    Route::post('/user/image/upload', [UserImageController::class, 'store'])->name('upload');
+    Route::get('/user/image/upload', [UserImageController::class, 'index']);
+    Route::get('/user/images', [UserImageController::class, 'showImages']);
+
+    Route::get('/dashboard', [AccountController::class, 'index']);
+
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
+
+Route::get('/article', [ArticleController::class, 'index']);
+Route::get('/articles', [ArticleController::class, 'all']);
+Route::post('/article/store', [ArticleController::class, 'store']);
+Route::post('/article/comment/add', [ArticleController::class, 'addComment']);
+Route::post('/article/like', [ArticleController::class, 'like']);
